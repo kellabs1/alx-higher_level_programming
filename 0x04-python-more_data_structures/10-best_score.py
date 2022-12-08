d29ec33902619e903b2ed76bdb0b38011892991f
kellabs1 <111052751+kellabs1@users.noreply.github.com>
2022-12-08 03:50:40 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        value = 0
-        for key in a_dictionary:
-            if value < a_dictionary[key]:
-                    value = a_dictionary[key]
-                    user = key
-        return user
-    else:
+    if a_dictionary is None:
         return None
+
+    b_score = max(a_dictionary.values(), default=None)
+    for k, v in a_dictionary.items():
+        if v == b_score:
+            return k
